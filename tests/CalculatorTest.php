a5f1da19e4fcbc767bc6f8088f02c37912effbbe
Nimisha Sreenivasan <nimisha.sreenivasan@sysfore.in>
2017-08-08 13:22:58 +0530

    Update CalculatorTest.php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -18,7 +18,7 @@ class CalculatorTests extends PHPUnit_Framework_TestCase
     public function testAdd()
     {
         $result = $this->calculator->add(1, 2);
-        $this->assertEquals(3, $result);
+        $this->assertEquals(85, $result);
     }
  
 }
